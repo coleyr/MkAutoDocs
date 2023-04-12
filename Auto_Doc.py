f5137783528094caa6225ca6f380dd44c92d52e3
Coley <Coangel@cisco.com>
2023-04-11 20:52:59 -0400

    updated the requirements, added encoding type, made a index generator.

--- Auto_Doc.py
+++ Auto_Doc.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 import re
 from dotenv import load_dotenv
-
+from typing import Callable
 #load configuration
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -50,6 +50,11 @@ def write_md_documentation(code: str) -> str:
     result = chat_completion(code, system)
     return result.choices[0].message["content"]
 
+def readme_md_summarization(code: str) -> str:
+    system = """You are a helpful documentation generation bot, you take in a README.md markdown file and return a summary page for a doc as code site in markdown format, create a title and organize the page so it is flows well and is pretty."""
+    result = chat_completion(code, system)
+    return result.choices[0].message["content"]
+
 def make_dir(path):
     print("Running mkdir")
     p = Path(path)
@@ -72,8 +77,22 @@ def ignore_folders(path_string:str):
 def make_save_folder(folder:str, root_dir:str, docs_path:str):
     rel_path = os.path.relpath(folder, root_dir)
     return Path(f"{root_dir}/{docs_path}") / rel_path
+
+def make_index(root_doc:str, docs_path:str, build_doc_function:Callable=readme_md_summarization):
+    readme_path = Path(root_doc, "README.md")
+    index_path = Path(root_doc, docs_path, "index.md")
+    if index_path.exists():
+        print('index.md already present not altering')
+        return
+    if not readme_path.exists():
+        index_path.write_text("", 'utf-8')
+        print('No readme and no index.md, creating a blank index.md')
+        return
+    text = Path(readme_path).read_text()
+    print(f"Building index from README.md: {readme_path}")
+    index_path.write_text(build_doc_function(text), 'utf-8')
     
-def get_file_documentation(files:list, save_path:Path)-> None:
+def get_file_documentation(files:list, save_path:Path, build_doc_function:Callable=write_md_documentation)-> None:
     make_dir(save_path)
     for file in files:
         if ignore_files(file):
@@ -85,7 +104,7 @@ def get_file_documentation(files:list, save_path:Path)-> None:
             continue
         text = Path(file).read_text()
         print(f"Getting documentation for file {doc_file_path}")
-        doc_file_path.write_text(write_md_documentation(text))
+        doc_file_path.write_text(build_doc_function(text), 'utf-8')
         
 def get_markdown(source_folder_dir:dir, save_folder:Path, root_doc:str, docs_path:str):
     files = source_folder_dir.get("files", [])
@@ -119,11 +138,11 @@ def make_mkdocs_yaml(root_dir:str)-> None:
 
 def update_requirements(requirement: str, version: str, file_path: str = "requirements.txt")-> None:
     # Read in the requirements file
-    if not Path(file_path).exists():
+    p = Path(file_path)
+    if not p.exists():
         print("creating requirements.txt")
-        Path(file_path).write_text("")
-    with open(file_path, "r") as f:
-        requirements = f.readlines()
+        p.write_text("", encoding='utf-8')
+    requirements = p.read_text('utf-8').splitlines()
     # Check if the requirement is already present and at the correct version
     for i, req in enumerate(requirements):
         if req.startswith(requirement):
@@ -132,16 +151,14 @@ def update_requirements(requirement: str, version: str, file_path: str = "requir
                 return
 
             # Update the requirement to the correct version
-            requirements[i] = f"{requirement}=={version}\n"
-            with open(file_path, "w") as f:
-                f.writelines(requirements)
+            requirements[i] = f"{requirement}=={version}"
+            p.write_text("\n".join(requirements), encoding='utf-8')
             print(f"Updated {requirement} to version {version}")
             return
 
     # If the requirement is not present, add it to the file
-    requirements.append(f"{requirement}=={version}\n")
-    with open(file_path, "w") as f:
-        f.writelines(requirements)
+    requirements.append(f"{requirement}=={version}")
+    p.write_text("\n".join(requirements), encoding='utf-8')
     print(f"Added {requirement} version {version} to requirements.txt")
 
 def make_mkdocs_documents():
@@ -152,9 +169,9 @@ def make_mkdocs_documents():
     already_parsed = build_state(root_and_docs_path) if use_state else set()
     print(f"Loaded state for {len(already_parsed)} files")
     for source_dir in source_to_document:
-        
         source_folder_dir = folder_structure(source_dir)
         get_markdown(source_folder_dir, root_and_docs_path, root_dir, docs_path)
+    make_index(root_dir, docs_path)
     make_mkdocs_yaml(root_dir)
     print("Building requirments.txt")
     for requirment in config.get("requirements"):
